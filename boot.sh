a90c8baf779d7187c55b531426126a447dcb3278
paulie <tempo.pause@gmail.com>
2015-12-04 15:31:25 +0100

    Testing boot up for back end

---
+++ boot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+services="backend"
+com="docker-compose"
+
+cd containers
+echo "Cleaning project containers (if any)"
+$com stop $services
+$com rm -f $services
+echo "Starting up"
+$com up -d $services
+if [ $? == "0" ]; then
+    echo "Up and running:"
+    $com ps
+    # docker volume ls
+fi
