225190a27ba00d7700125fda5977bec0d68cd50b
paulie <tempo.pause@gmail.com>
2015-12-11 15:02:47 +0100

    Moved test code into an angulask module. Looking good.

--- vanilla/pages/example.py
+++ vanilla/pages/example.py
@@ -9,61 +9,19 @@ You need to define a Blueprint called 'restbp' to make this work.
 """
 
 from __future__ import absolute_import
-from flask import Blueprint, \
-    render_template, make_response
-from jinja2 import TemplateNotFound
-from flask_restful import Api, Resource
+from ..views import RestView, generate_blueprint
 
 """
 // TO FIX:
 
+* Make dynamic assignment of all endpoints on all classes
+    - check only the ones which derive from RestView
+* Make dynamic load of blueprints inside 'create_app' ...??
 * Choose a dynamic template folder
     - based on blueprint name?
     - use variable from config to give the base
-* Move base rest view class somewhere
-    - also move imports
-* Make dynamic assignment of all endpoints on all classes
-    - check only the ones which derive from RestView
 
 """
-
-################################
-# Using restful blueprint for all classes
-bp = Blueprint('someviews', __name__,
-               template_folder='templates')
-# Use restful on current blueprint
-rest = Api(bp)
-
-
-# ################################
-# # NORMAL WAY
-# @bp.route('/', defaults={'page': 'index'})
-# @bp.route('/<page>')
-# def show(page):
-#     try:
-#         return render_template('pages/%s.html' % page)
-#     except TemplateNotFound:
-#         abort(404)
-
-
-################################
-# REST WAY
-class RestView(Resource):
-    """ A base REST resource for views """
-    _headers = {'Content-Type': 'text/html'}
-    _endpoint = 'test'
-
-    def __init__(self):
-        pass
-
-    def endpoint(self):
-        return '/' + self.__class__.__name__.lower()
-
-    def render(self, page='test.html'):
-        try:
-            return make_response(render_template(page), 200, self._headers)
-        except TemplateNotFound:
-            return make_response("Failed", 404, self._headers)
 
 
 # Restful view class
@@ -72,10 +30,6 @@ class ToDo(RestView):
     def get(self):
         return self.render('test.html')
 
-# Add resources
-rest.add_resource(ToDo, ToDo().endpoint())
-# rest.add_resource(ToDo, '/todos/<int:id>')
-################################
-
-# WHAT TO DO LATER WITH BLUEPRINT:
-# app.register_blueprint(bp)
+bp = generate_blueprint(__name__,
+                        classes=[ToDo],
+                        folder='/data/angulask')
