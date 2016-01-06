9a362db8e25f1107cc7a0bcdbf3a565d8e610d8d
Paulie D <p.donoriodemeo@gmail.com>
2016-01-06 01:19:46 +0100

    Completing templates for search and details.

--- vanilla/jscode/base/index.root.controller.js
+++ vanilla/jscode/base/index.root.controller.js
@@ -14,12 +14,17 @@ function AppRootController($scope, $log, $state)
 
 	forEach(myObj, function (x, i) {
 		var key = 'logged.'
-		//console.log(x.name.slice(0, key.length));
-		if (x.name.slice(0, key.length) == key) {
-// TO FIX:
-// if state has mandatory params, skip it from MENU
-			$scope.menu.push(x.name.substr(7));
-		}
+        var prefix = x.name.slice(0, key.length);
+        var suffix = x.name.slice(key.length);
+        // Skip if: 
+            // a - the view is a sub child
+            // b - there is a parameter into the url (so direct link won't work)
+        if (suffix.indexOf('.') < 0 && x.url.indexOf(':') < 0) {
+            console.log(x, i, suffix, suffix.indexOf('.'));
+    		if (prefix == key) {
+    			$scope.menu.push(x.name.substr(7));
+    		}
+        }
 	});
 
 	$log.info("Menu", $scope.menu);
