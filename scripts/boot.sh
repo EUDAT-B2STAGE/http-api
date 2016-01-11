ffbd0f2a2d16cc776762a2a2a805b6178090f159
pdonorio <tempo.pause@gmail.com>
2016-01-11 12:24:47 +0100

    Custom script to boot service

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #############################
-extra="-f docker-compose.yml -f custom.yml"
-com="docker-compose $extra"
+com="docker-compose"
 #services="backend frontend"
 services="custombe customfe"
 webbuild="bower"
@@ -53,15 +52,28 @@ elif [ "$1" == "bower" ]; then
 
 # Launch services
 else
+
+    if [ -z "$1" ]; then
+        echo "Please specify one configuration to use."
+        echo "Available configurations:"
+        for i in `ls custom`;
+        do
+            name=`echo $i | sed "s/\.yml//"`
+            echo -e "\t$name"
+        done
+        exit
+    fi
+    files="-f docker-compose.yml -f custom/${1}.yml"
+
     #############################
     echo "Cleaning project containers (if any)"
-    $com stop $services
-    $com rm -f $services
+    $com $files stop $services
+    $com $files rm -f $services
     echo "Starting up"
-    $com up -d $services
+    $com $files up -d $services
     if [ "$?" == "0" ]; then
         echo "Up and running:"
-        $com ps
+        $com $files ps
         # docker volume ls
     fi
 fi
