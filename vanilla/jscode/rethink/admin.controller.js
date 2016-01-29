0edd5729d8bb17adebc1c2b35d98cd93059c650d
pdonorio <tempo.pause@gmail.com>
2016-01-29 09:29:52 +0100

    Working the idea of dialog as new create element

--- vanilla/jscode/rethink/admin.controller.js
+++ vanilla/jscode/rethink/admin.controller.js
@@ -2,9 +2,23 @@
   'use strict';
 
 angular.module('web')
+    .controller('DialogController', DialogController)
     .controller('AdminController', AdminController);
 
-function AdminController($scope, $rootScope, $log, admin)
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
+
+
+function AdminController($scope, $rootScope, $log, admin, $mdMedia, $mdDialog)
 {
   // Init controller
   var self = this;
@@ -78,6 +92,31 @@ function AdminController($scope, $rootScope, $log, admin)
   self.onTabSelected = function () {
       $log.debug("Selected", self.selectedTab);
   }
+
+  $scope.status = '  ';
+  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+
+  $scope.showAdvanced = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: blueprintTemplateDir + 'add_section.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+  };
 
 }
 
