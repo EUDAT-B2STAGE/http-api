5d55d86f72a1fd015d4b1162dcc2ebd9d4ac7087
pdonorio <tempo.pause@gmail.com>
2016-02-05 15:05:27 +0100

    A draft for history inside a sidebar

--- vanilla/jscode/commons/common.rootctrl.js
+++ vanilla/jscode/commons/common.rootctrl.js
@@ -10,38 +10,29 @@ angular.module('web')
 function HistorySidebarController($scope, $log, $mdSidenav, $mdComponentRegistry)
 {
     var self = this;
-    self.name = "right";
+    self.name = "mysnav";
     $log.debug("Sidebar history controller");
 
-/* WORKS
-        //http://luxiyalu.com/angular-material-no-instance-found-for-handle-left/
+    self.history = getHistoryOfAllTimes();
+
+    // SIDEBAR STRANGE BEHAVIOUR
+    //http://luxiyalu.com/angular-material-no-instance-found-for-handle-left/
+
+    $scope.open = function () {
         $mdComponentRegistry.when(self.name).then(function(sidenav){
           sidenav.open();
         });
-*/
-
-/* WORKS NOT!!!
-    $mdSidenav(self.name).toggle()
-          .then(function () {
-            $log.debug("toggle " + self.name + " is done");
-          });
-    $scope.isOpenRight = function(){
-      return $mdSidenav(self.name).isOpen();
     };
-*/
 
     //$mdSidenav(self.name).open().then();
     $scope.close = function () {
         $mdComponentRegistry.when(self.name).then(function(sidenav){
           sidenav.close();
         });
-/*
-      $mdSidenav(self.name).close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-*/
     };
+
+// TO FIX
+    $scope.open();
 }
 function ToolbarController($scope, $log, $rootScope)
 {
