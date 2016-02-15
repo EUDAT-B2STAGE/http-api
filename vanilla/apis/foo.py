be366a25a131427112b345e868177fe20d6a56b7
pdonorio <tempo.pause@gmail.com>
2016-02-15 13:50:00 +0100

    New docker image to connect to graph. Library for neo4j has moved to backend.

--- vanilla/apis/foo.py
+++ vanilla/apis/foo.py
@@ -7,8 +7,9 @@ An endpoint example
 from restapi import get_logger
 from ..base import ExtendedApiResource
 from .. import decorators as decorate
-from .myrods import mirods
-from .mygraph import migraph
+#from .myrods import mirods
+#from .mygraph import migraph
+from ..services.neo4j import migraph
 
 # AUTH
 # from confs import config
@@ -23,8 +24,8 @@ class PidHandle(ExtendedApiResource):
     @decorate.apimethod
     def get(self):
         """ Get pid """
-        logger.warning("irods call %s", mirods.other())
+        #logger.warning("irods call %s", mirods.other())
         logger.warning("graph call %s", migraph.other())
         query = "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r"
-        migraph.cipher(query)
+        migraph.cypher(query)
         return self.response('Hello PID!')
