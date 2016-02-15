b7b223b37e8f2a22e51c5421de1b003b6bd773d8
pdonorio <tempo.pause@gmail.com>
2016-02-15 23:27:06 +0100

    Adding an irods small test which fails

--- vanilla/apis/eudat.py
+++ vanilla/apis/eudat.py
@@ -8,14 +8,13 @@ from ..base import ExtendedApiResource
 from .. import decorators as decorate
 
 from ..services.neo4j import migraph
-from ..services.irodsclient import mirods
+from ..services.irodsclient import ICommands
 
 # AUTH
 # from confs import config
 # from flask.ext.security import roles_required, auth_token_required
 
 from restapi import get_logger
-
 logger = get_logger(__name__)
 
 
@@ -25,7 +24,8 @@ class PidHandle(ExtendedApiResource):
     @decorate.apimethod
     def get(self):
         """ Get pid """
-        logger.warning("irods call %s", mirods.other())
+        icom = ICommands()
+        logger.warning("irods call %s", icom.list())
         logger.warning("graph call %s", migraph.other())
         query = "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r"
         migraph.cypher(query)
