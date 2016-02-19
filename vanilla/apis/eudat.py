9b3ef4e72ac23738cb2f9bbe088fc96f3f2aeb2a
pdonorio <tempo.pause@gmail.com>
2016-02-19 08:11:11 +0100

    Changing endpoint name and adding a mock post for uploading a file

--- vanilla/apis/eudat.py
+++ vanilla/apis/eudat.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """
-An endpoint example
+A EUDAT endpoint example.
+
+To discuss:
+- sqlite db for users and administration
+    * pluggable authentication
+- upload
+    * progress
+    * streaming
+-
 """
 
 from ..base import ExtendedApiResource
+from flask.ext.restful import request
+#from werkzeug import secure_filename
 from .. import decorators as decorate
 
 from ..services.neo4j import migraph
@@ -19,10 +29,10 @@ logger = get_logger(__name__)
 
 
 #####################################
-class PidHandle(ExtendedApiResource):
+class DataObjectEndpoint(ExtendedApiResource):
 
     @decorate.apimethod
-    def get(self):
+    def get(self, id=None):
         """ Get pid """
 
         # GraphDB
@@ -34,4 +44,20 @@ class PidHandle(ExtendedApiResource):
         # #logger.info("irods call %s", icom.list())
         logger.info("irods call %s", icom.change_user('guest'))
 
-        return self.response('Hello PID!')
+        return self.response(
+            'There should be one or more data object here in response')
+
+    @decorate.apimethod
+    def post(self):
+        """ Upload a file """
+
+        if 'file' not in request.files:
+            return "No files specified"
+
+        myfile = request.files['file']
+        #filename = secure_filename(myfile.filename)
+        #destination = MYDIR + filename
+        #myfile.save(destination)
+
+        return self.response(
+            "The file to be uploaded is '%s'" % myfile)
