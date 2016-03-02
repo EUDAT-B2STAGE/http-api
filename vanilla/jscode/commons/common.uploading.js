7aac2064e71e02e9552bcf630fc73548f1f9d86c
pdonorio <tempo.pause@gmail.com>
2016-03-02 15:26:31 +0100

    Updating the template of this uploader dialog

---
+++ vanilla/jscode/commons/common.uploading.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+angular.module('web')
+    .controller('UploadController', UploadController);
+
+
+function UploadController($scope, $log, $mdDialog)
+{
+    var self = this;
+    $log.debug("Uploader");
+
+    self.validate = function() {
+      $mdDialog.hide('OK');
+    };
+
+    self.cancel = function() {
+      $mdDialog.hide('FAIL');
+    };
+
+};
+
+})();
--- vanilla/jscode/rethink/explore.controller.js
+++ vanilla/jscode/rethink/explore.controller.js
@@ -68,21 +68,22 @@ function FixImagesController($scope, $log, $mdDialog)
       self.currentParty = name;
     }
 
+
 /////////////////////////////////////
     self.uploaderDialog = function()
     {
       var dialogOptions = {
-          template: "This will be the uploader dialog",
-          //controller: DialogController,
-          parent: angular.element(document.body),
-          //locals: { id: null, },
-          //targetEvent: ev,
-          clickOutsideToClose:true,
-          //onComplete: function() { $log.debug("complete"); },
+        templateUrl: blueprintTemplateDir + 'uploader.html',
+        //controller: UploadController,
+        parent: angular.element(document.body),
+        //locals: { id: null, },
+        //targetEvent: ev,
+        clickOutsideToClose:true,
+        //onComplete: function() { $log.debug("complete"); },
         }
 
       var afterDialog = function(response) {
-            $log.info("Dialog response", response);
+        $log.info("Dialog response", response);
       }
 
       // Open
