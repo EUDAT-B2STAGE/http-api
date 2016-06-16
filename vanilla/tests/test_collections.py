4d9a76c760a855c324c51e263b6af7dd6f34c684
pdonorio <tempo.pause@gmail.com>
2016-06-17 00:09:59 +0200

    Starting a 'collection' set of tests

--- vanilla/tests/test_collections.py
+++ vanilla/tests/test_collections.py
@@ -1,36 +1,64 @@
 # -*- coding: utf-8 -*-
 
 """
-Test  dataobjects endpoints
+Test collection endpoints.
+
+Run single test:
+nose2 test.custom.test_collections.TestCollections.test_01_get_collections
+
 """
 
-# import io
-# import os
-# import json
+from __future__ import absolute_import
+
+import os
+import json
 import unittest
+import commons.htmlcodes as hcodes
 from restapi.server import create_app
+from restapi.confs.config import USER, PWD, \
+    TEST_HOST, SERVER_PORT, API_URL, AUTH_URL
 
+from commons.logs import get_logger
+from commons import myself
 
-__author__ = 'Roberto Mucci (r.mucci@cineca.it)'
+__author__ = myself
+
+logger = get_logger(__name__, True)
+
+API_URI = 'http://%s:%s%s' % (TEST_HOST, SERVER_PORT, API_URL)
+AUTH_URI = 'http://%s:%s%s' % (TEST_HOST, SERVER_PORT, AUTH_URL)
 
 
 class TestCollections(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
-        "set up test fixtures"
-        print('### Setting up flask server ###')
+    def setUpClass(cls):
+        logger.info('### Setting up flask server ###')
         app = create_app(testing=True)
-        # app.config['TESTING'] = True
-        self.app = app.test_client()
+        cls.app = app.test_client()
+
+        r = cls.app.post(
+            AUTH_URI + '/login',
+            data=json.dumps({'username': USER, 'password': PWD}))
+        content = json.loads(r.data.decode('utf-8'))
+        cls.auth_header = {
+            'Authorization': 'Bearer ' + content['Response']['data']['token']}
 
     @classmethod
-    def tearDownClass(self):
-        "tear down test fixtures"
-        print('### Tearing down the flask server ###')
+    def tearDownClass(cls):
+        logger.info('### Tearing down the flask server ###')
+        del cls.app
 
-    # def test_01_get_status(self)
-    #     """ Test that the flask server is running and reachable"""
+        # Tokens clean up
+        logger.debug("Cleaned up invalid tokens")
+        from restapi.resources.services.neo4j.graph import MyGraph
+        MyGraph().clean_pending_tokens()
 
-    #     r = self.app.get('http://localhost:8080/api/status')
-    #     self.assertEqual(r.status_code, 200)
+    def test_01_get_collections(self):
+        """ Test """
+
+        URI = os.path.join(API_URI, 'collections')
+        r = self.app.get(URI, headers=self.auth_header)
+        self.assertEqual(r.status_code, hcodes.HTTP_OK_BASIC)
+        out = json.loads(r.data.decode('utf-8'))
+        self.assertIsInstance(out['Response']['data']['content'], list)
