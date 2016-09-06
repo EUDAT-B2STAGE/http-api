721a292ba40abcb491ce8b0a5c02a859468e870b
pdonorio <tempo.pause@gmail.com>
2016-09-07 01:00:13 +0200

    Some fix for roberto

--- vanilla/apis/internals/metadata.py
+++ vanilla/apis/internals/metadata.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 
 from commons.logs import get_logger
 from commons.services.uuid import getUUID
+from ..services.irods.client import IRODS_DEFAULT_USER
 from ...base import ExtendedApiResource
 from ... import decorators as decorate
 from ....auth import authentication
@@ -16,46 +17,45 @@ from ....confs import config
 logger = get_logger(__name__)
 
 
+## // TO FIX:
+# Use the correct irods user from the token,
+# instad of IRODS_DEFAULT_USER
+irods_tmp_user = IRODS_DEFAULT_USER
+
+
 class MetaDataParser(ExtendedApiResource):
 
     @authentication.authorization_required(config.ROLE_INTERNAL)
     @decorate.apimethod
-    def put(self):
+    def put(self, mid):
 
         # Get neo4j service object
         graph = self.global_get_service('neo4j')
 
-        # Do irods things
-        icom = self.global_get_service('irods', user=irods_tmp_user)
-
         # Get filename and ipath from uuid using the graph
         try:
-            de_node = graph.DigitalEntity.nodes.get(id=uuid)
+            de_node = graph.DigitalEntity.nodes.get(id=mid)
         except graph.DigitalEntity.DoesNotExist:
-            return self.force_response(errors={uuid: 'Not found.'})
+            return self.force_response(errors={mid: 'Not found.'})
 
-        manifest_path == None
-        manifest_path = this._recursive_search(de_node)
+        manifest_path = None
+        manifest_path = self._recursive_search(de_node)
 
         if manifest_path is None:
             #except graph.DigitalEntity.DoesNotExist: # to be modified
             return self.force_response(errors={'manifest.xml not found.'})
 
-
-
         # Recursivly search for manifest.xml
 
+        # Do irods things
+        icom = self.global_get_service('irods', user=irods_tmp_user)
 
-
-        # irods paths
-        ipath = icom.get_irods_path(
-            collection_node.path, dataobj_node.filename)
+        # # irods paths
+        # ipath = icom.get_irods_path(
+        #     collection_node.path, de_node.filename)
 
         # 2. Find the manifest file
         # 3. Parse the manifest file
-
-
-        
 
         ###########################
         # Models can be found inside the graphdb object
@@ -89,7 +89,7 @@ class MetaDataParser(ExtendedApiResource):
                     this._recursive_search(de)
         else:
             if de.name == 'manifest.xml':
-                
+
                 return de.location
             else:
                 print('manifest.xml not found')
