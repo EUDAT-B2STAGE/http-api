5ee56180bbe1cae9bc78ccbbc4569da520cf2ab3
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-27 07:22:41 +0200

    Removing demo project

--- js/graphbased/welcome.controller.js
+++ js/graphbased/welcome.controller.js
@@ -1,1 +1,13 @@
-../demo/welcome.controller.js
+(function() {
+  'use strict';
+
+angular.module('web')
+    .controller('WelcomeController', WelcomeController);
+
+function WelcomeController($scope, $log)
+{
+  $log.debug("Welcome controller", $stateParams);
+  self.init = 'example';
+}
+
+})();
