80488a2a867c35e7983d18e91536ab62891a309a
m.dantonio@cineca.it <m.dantonio@cineca.it>
2016-09-27 07:08:07 +0200

    removed material, added bootstrap

--- js/graphbased/app.js
+++ js/graphbased/app.js
@@ -1,52 +1,15 @@
 (function() {
   'use strict';
 
-/* Define another module?
-console.log('PRE');
-angular.module('blueprint', ['ui.router']);
-console.log('POST');
-*/
-
   angular
     .module('web', [
-// BASE
-        //'ngAnimate',
         'ngSanitize',
-// ROUTING
-        'ui.router', //'ngRoute',
-// BOOTSTRAP
-        //'mgcrea.ngStrap',
-// OFFICIAL MATERIAL
-        'ngMaterial',
-// MATERIAL DESIGN
-        'ui.materialize',
-// AUTH
+        'ui.router',
+        'ui.bootstrap',
         'satellizer',
-// KEYBOARD SHORTCUTS
         'cfp.hotkeys',
-// FORMS FROM JSON SCHEMAS
         'formly',
-        'formlyBootstrap',
-//////////////////////////////////////
-/* Custom */
-
-// THE LOADING BAR
-        //'angular-loading-bar',
-        //'cfp.loadingBar',
-// IMAGES LAZ LOAD
-        // 'angularLazyImg',
-// MAKE DATA TREE explorer
-        // 'treeControl',
-// DRAG AND DROP
-        // 'angular-sortable-view',
-// EDITABLE FORMS
-        // 'xeditable',
-// RESTARTABLE UPLOAD
-        // 'flow',
-
-/* Custom end */
-//////////////////////////////////////
-
+        'formlyBootstrap'
     ]);
 
 })();
