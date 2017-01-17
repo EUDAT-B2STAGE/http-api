0ff19b116d9981875d610bf587d2c4326c48edeb
pdonorio <tempo.pause@gmail.com>
2017-01-17 17:22:51 +0100

    Adding the jwt secret at init time

--- confs/fixirodsclient.sh
+++ confs/fixirodsclient.sh
@@ -8,4 +8,7 @@ chown -R $user /dbs /uploads
 # mypath="/var/lib/irods/plugins/auth"
 # ln -s /sharedlibs$mypath/libgsi.so $mypath
 
+# Create the secret to enable security on JWT tokens (in production)
+head -c 24 /dev/urandom > /jwt_tokens/secret.key
+
 echo "Completed"
