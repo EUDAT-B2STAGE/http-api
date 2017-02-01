5bf84c15f12584f7d0ed9bfca7d72c38ea21b9db
pdonorio <tempo.pause@gmail.com>
2017-02-01 14:37:11 +0100

    Switching to postgresql in case of production mode.
    See #25

--- confs/pgs_init.sh
+++ confs/pgs_init.sh
@@ -5,9 +5,25 @@ conf='/var/lib/postgresql/data/pg_hba.conf'
 
 echo "Changing access"
 echo "" > $conf
-#echo "local   $POSTGRES_USER  $POSTGRES_USER  trust" >> $conf  #ONLY DEBUG
+# ENABLE THIS ONLY FOR LOCAL DEBUG
+# echo "local   $POSTGRES_USER  $POSTGRES_USER  trust" >> $conf
 echo "hostnossl       postgres  $POSTGRES_USER  172.17.0.0/16   password" >> $conf
-echo "hostnossl       $POSTGRES_DB  $POSTGRES_USER  172.17.0.0/16   password" >> $conf
+
+###################
+# DBs handling
+for db in $POSTGRES_DBS;
+do
+    echo "Enabling DB $db"
+# Create it
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" << EOSQL
+    CREATE DATABASE "$db";
+EOSQL
+    # GRANT ALL PRIVILEGES ON DATABASE "$db" TO $POSTGRES_USER;
+# Add privileges
+echo "hostnossl       $db  $POSTGRES_USER  172.17.0.0/16   password" >> $conf
+done
+
+###################
 echo "DONE"
 
 ## In case you need to startup a database
