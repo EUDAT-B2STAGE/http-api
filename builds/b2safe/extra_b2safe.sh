b430c782b4da4a75221721a8da143d95895f402c
pdonorio <tempo.pause@gmail.com>
2017-03-11 15:50:32 +0100

    Adding the graphdb to make tests in refactoring service injections.

--- builds/b2safe/extra_b2safe.sh
+++ builds/b2safe/extra_b2safe.sh
@@ -6,3 +6,6 @@ add-irods-X509 rodsminer admin
 
 # # More?
 # echo "Extra!"
+
+echo -e "echo\\necho 'Become irods administrator user with the command:'\\necho '$ berods'" >> /root/.bashrc
+echo -e "echo\\necho 'Switch irods GSI user then with the command:'\\necho '$ switch-gsi USERNAME'\\necho" >> /root/.bashrc
