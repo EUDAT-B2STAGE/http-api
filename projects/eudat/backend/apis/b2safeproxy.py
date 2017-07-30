eef5bc5c07fd1cf468c2b589e85b5faadc25108a
pdonorio <tempo.pause@gmail.com>
2017-07-31 00:41:34 +0200

    Completing serious unit tests on b2safeproxy

--- projects/eudat/backend/apis/b2safeproxy.py
+++ projects/eudat/backend/apis/b2safeproxy.py
@@ -8,7 +8,6 @@ from restapi import decorators as decorate
 from restapi.rest.definition import EndpointResource
 from restapi.exceptions import RestApiException
 from restapi.flask_ext.flask_irods.client import get_and_verify_irods_session
-from restapi.services.detect import detector
 from utilities import htmlcodes as hcodes
 from utilities.logs import get_logger
 
@@ -19,23 +18,21 @@ class B2safeProxy(EndpointResource):
 
     @decorate.catch_error()
     def get(self):
-        response = 'Hello world!'
 
-        #############
         user = self.auth.get_user()
         if user.session is not None and len(user.session) > 0:
             log.debug("Valid B2SAFE user: %s" % user.uuid)
         else:
-            raise ValueError("This user is not registered inside B2SAFE")
+            msg = "This user is not registered inside B2SAFE"
+            raise RestApiException(
+                msg, status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
 
         icom = self.get_service_instance(
             service_name='irods',
             user_session=user,
         )
-        print("Working with", icom)
-
-        #############
-        return self.force_response(response)
+        icom.list()
+        return 'validated'
 
     @decorate.catch_error()
     def post(self):
@@ -50,23 +47,21 @@ class B2safeProxy(EndpointResource):
             raise RestApiException(
                 msg, status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
 
-        # #############
-        # security = HandleSecurity(self.auth)
-        # security.verify_blocked_username(username)
-
-        #############
         func = self.get_service_instance
         params = {
             'service_name': "irods",
             'user': username,
             'password': password,
         }
+
         # we verify that irods connects with this credentials
         irods = get_and_verify_irods_session(func, params)
         if irods is None:
-            raise AttributeError("Failed to authenticate on B2SAFE")
+            msg = "Failed to authenticate on B2SAFE"
+            raise RestApiException(
+                msg, status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
         else:
-            encoded_session = irods.rpc.serialize()
+            encoded_session = irods.prc.serialize()
 
         token = self.auth.irods_user(username, encoded_session)
         return {'token': token}
--- projects/eudat/backend/tests/test_b2safeproxy.py
+++ projects/eudat/backend/tests/test_b2safeproxy.py
@@ -14,46 +14,78 @@ log = get_logger(__name__)
 class TestB2safeProxy(RestTestsAuthenticatedBase):
 
     _main_endpoint = '/b2safeproxy'
+    _irods_service = 'irods'
     _irods_user = 'icatbetatester'
     _irods_password = 'IAMABETATESTER'
 
     def setUp(self):
-        super().setUp()
-        log.warning("Paolo START")
+        """
+        add a user for irods to be tested with direct credentials
+        """
 
-        # iadmin mkuser paolo rodsuser
-        # iadmin moduser paolo password tester
+        super().setUp()
+        log.debug('### Creating custom data ###\n')
+        # how to get a service before having any app request
+        i = self.get_service_handler(self._irods_service)
+        # create a dedicated irods user and set the password
+        if i.create_user(self._irods_user):
+            i.modify_user_password(self._irods_user, self._irods_password)
 
     def tearDown(self):
-        # log.debug('### Cleaning custom data ###\n')
-        log.warning("Paolo END")
+        """
+        remove irods user created only for testing
+        """
+
+        log.debug('### Cleaning custom data ###\n')
+        i = self.get_service_handler(self._irods_service)
+        i.remove_user(self._irods_user)
         super().tearDown()
 
-    def test_01_whatever(self):
-        """
-        To create a user and test it:
-        nose2 -F tests.custom.test_b2safeproxy
-        """
+    def test_01_b2safe_login(self):
 
         endpoint = (self._auth_uri + self._main_endpoint)
         log.info('*** Testing auth b2safe on %s' % endpoint)
         r = self.app.post(
             endpoint,
-            data=dict(username='paolo', password='tester')
+            data=dict(
+                username=self._irods_user, password=self._irods_password
+            )
         )
 
         self.assertEqual(r.status_code, self._hcodes.HTTP_OK_BASIC)
-        json_output = json.loads(r.get_data(as_text=True))
-        data = json_output.get('Response', {}).get('data', {})
+        data = self.get_content(r)
         key = 'token'
         self.assertIn(key, data)
         token = data.get(key)
-        print(token[0])
 
         # verify that token is valid
-
-        # verify that normal token from normal login is invalid
+        r = self.app.get(
+            endpoint, headers={'Authorization': 'Bearer %s' % token})
+        self.assertEqual(r.status_code, self._hcodes.HTTP_OK_BASIC)
+        self.assertEqual('validated', self.get_content(r))
 
         # verify wrong username or password
+        r = self.app.post(
+            endpoint, data=dict(
+                username=self._irods_user + 'a',
+                password=self._irods_password
+            )
+        )
+        self.assertEqual(r.status_code, self._hcodes.HTTP_BAD_UNAUTHORIZED)
+        r = self.app.post(
+            endpoint, data=dict(
+                username=self._irods_user,
+                password=self._irods_password + 'b'
+            )
+        )
+        self.assertEqual(r.status_code, self._hcodes.HTTP_BAD_UNAUTHORIZED)
 
         # verify random token
+        wrong_token = token.lower().replace('e', 'Z').replace('j', 'i')
+        r = self.app.get(
+            endpoint, headers={'Authorization': 'Bearer %s' % wrong_token})
+        self.assertEqual(r.status_code, self._hcodes.HTTP_BAD_UNAUTHORIZED)
+
+        # verify that normal token from normal login is invalid
+        r = self.app.get(endpoint, headers=self.__class__.auth_header)
+        self.assertEqual(r.status_code, self._hcodes.HTTP_BAD_UNAUTHORIZED)
