7bdfd3cdbf569ab226747459f342f9971bf3aa8f
massimo1962 <massimo.fares@ingv.it>
2017-12-06 15:00:54 +0100

    first class airods: data&pid

--- projects/b2stage/backend/apis/airods.py
+++ projects/b2stage/backend/apis/airods.py
@@ -9,6 +9,7 @@ Endpoint example for the RAPyDo framework
 from restapi.rest.definition import EndpointResource
 from restapi.services.detect import detector
 from utilities.logs import get_logger
+import dateutil.parser
 
 #################
 # INIT VARIABLES
@@ -23,46 +24,78 @@ service_name = "sqlalchemy"
 class Airods(EndpointResource):
 
     def get(self):
-        # Write server logs, on different levels:
-        # very_verbose, verbose, debug, info, warning, error, critical, exit
-        log.info("Received a test HTTP request")
-
+        # # --> important into mongo collections we must have:
+        # #     "_cls" : "b2stage.models.mongo.wf_do"
+        
         service = 'mongo'
         mongohd = self.get_service_instance(service_name=service)
-        # from restapi.services.detect import detector
+        
         variables = detector.output_service_variables(service)
         db = variables.get('database')
         # test if it works
         # BAD: using private internal meta
         # (src: http://bit.ly/2njr3LN)
-        mongohd.Testing._mongometa.connection_alias = db
-        mongohd.Testing(onefield='justatest1').save()
-        for element in mongohd.Testing.objects.all():
-            log.pp(element)
+        mongohd.wf_do._mongometa.connection_alias = db
+        
+        # just a test:
+        # mongohd.Testing(onefield='justatest1').save()
+        # for element in mongohd.Testing.objects.all():
+        #    log.pp(element)
+        # return "Completed"
+        
+        
+        # real:
+        myargs = self.get_input()
+        print(myargs)
+        documentResult1 = []
+        
+        mycollection = mongohd.wf_do
+        
+        myStartDate = dateutil.parser.parse(myargs.get("start"))
+        myEndDate = dateutil.parser.parse(myargs.get("end"))
+        myLat = float(myargs.get("minlat"))
+        myLon = float(myargs.get("minlon"))
+        myLatX = float(myargs.get("maxlat"))
+        myLonX = float(myargs.get("maxlon"))
 
-        """
-        # Parse input parameters:
-        # NOTE: they can be caught only if indicated in swagger files
-        self.get_input()
-        # pretty print arguments obtained from the _args private attribute
-        log.pp(self._args, prefix_line='Parsed args')
+        #myfirstvalue = mycollection.objects.raw({"dc_coverage_x": { "$gte" : myLat }, "dc_coverage_y": { "$gte" : myLon }, "dc_coverage_x": { "$lte" : myLatX }, "dc_coverage_y": { "$lte" : myLonX },"dc_coverage_t_min": { "$gte" : myStartDate },"dc_coverage_t_max": { "$lte" : myEndDate }})
+        myfirstvalue = mongohd.wf_do.objects.all()
+        for document in myfirstvalue:
+                myLine = [document.fileId,document.dc_identifier,document.irods_path, document.dc_coverage_x, document.dc_coverage_y ]
+                documentResult1.append(myLine)
 
-        # Activate a service handle
-        service_handle = self.get_service_instance(service_name)
-        log.debug("Connected to %s:\n%s", service_name, service_handle)
+        
+        # Download :: to check w/ irods
+        if myargs.get('download') == 'true':
+            
+            icom = self.get_service_instance(service_name='irods') #, user='rods', password='sdor' 
+            
+            myobj = icom.list() #myfirstvalue[0].irods_path
+            #myresp = icom.get_permissions(myfirstvalue[0].irods_path)
+            print (myobj)
+            
+            #for meta in my-meta:
+             #   print(meta)
+            
+            print ("eccolo")
+            print (myfirstvalue[0].irods_path) 
+                
+            try:
+                for document in myfirstvalue:
+                    print(document.irods_path)
+                    icom.read_in_streaming(document.irods_path)
+                    
+                
+                return("download ok")
+            
+            except BaseException as e:
+                print(e, type(e))
+                return self.force_response('errore')
+                
+            
+        # Pid list :: OK
+        else:
+            
+            return  ["total files to download: "+str(len(documentResult1)) +" format:< fileId - PID - Lat - Lon >",documentResult1]   # ,documentResult1
 
-        # Handle errors
-        if service_handle is None:
-            log.error('Service %s unavailable', service_name)
-            return self.send_errors(
-                message='Server internal error. Please contact adminers.',
-                # code=hcodes.HTTP_BAD_NOTFOUND
-            )
-
-        # Output any python structure (int, string, list, dictionary, etc.)
-        response = 'Hello world!'
-        return self.force_response(response)
-        """
-
-        return "Completed"
 
--- projects/b2stage/backend/models/mongo.py
+++ projects/b2stage/backend/models/mongo.py
@@ -15,3 +15,29 @@ class Testing(MongoModel):
     #     # write_concern = WriteConcern(j=True)
 
 # FIXME: two fields are missing in ExternalAccounts
+
+class wf_do(MongoModel):
+    dc_identifier = fields.CharField()
+    dc_title = fields.CharField()
+    dc_subject = fields.CharField()
+    dc_creator = fields.CharField()
+    dc_contributor = fields.CharField()
+    dc_publisher = fields.CharField()
+    dc_type = fields.CharField()
+    dc_format = fields.CharField()
+    dc_date = fields.DateTimeField()
+    dc_coverage_x = fields.FloatField()
+    dc_coverage_y = fields.FloatField()
+    dc_coverage_z = fields.FloatField()
+    dc_coverage_t_min = fields.DateTimeField()
+    dc_coverage_t_max = fields.DateTimeField()
+    dcterms_available = fields.DateTimeField()
+    dcterms_dateAccepted = fields.DateTimeField()
+    dc_rights = fields.CharField()
+    dcterms_isPartOf = fields.CharField()
+    fileId = fields.CharField()
+    irods_path = fields.CharField()
+
+    #class Meta:
+    #    write_concern = WriteConcern(j=True)
+    #    connection_alias = MYDB
