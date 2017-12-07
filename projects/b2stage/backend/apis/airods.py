03e2f522751ff7ed8331cbe8554de9586930810f
massimo1962 <massimo.fares@ingv.it>
2017-12-07 13:07:03 +0100

    second class airods: dublin-core metadata

--- projects/b2stage/backend/apis/airods.py
+++ projects/b2stage/backend/apis/airods.py
@@ -99,3 +99,103 @@ class Airods(EndpointResource):
             return  ["total files to download: "+str(len(documentResult1)) +" format:< fileId - PID - Lat - Lon >",documentResult1]   # ,documentResult1
 
 
+#################
+# REST CLASS
+class AirodsMeta(EndpointResource):
+
+    def get(self):
+
+       # # --> important into mongo collections we must have:
+        # #     "_cls" : "b2stage.models.mongo.wf_do"
+        
+        service = 'mongo'
+        mongohd = self.get_service_instance(service_name=service)
+        
+        variables = detector.output_service_variables(service)
+        db = variables.get('database')
+        # test if it works
+        # BAD: using private internal meta
+        # (src: http://bit.ly/2njr3LN)
+        mongohd.wf_do._mongometa.connection_alias = db
+        
+        # just a test:
+        # mongohd.Testing(onefield='justatest1').save()
+        # for element in mongohd.Testing.objects.all():
+        #    log.pp(element)
+        # return "Completed"
+        
+        # real:
+        myargs = self.get_input()
+        print(myargs)
+        documentResult1 = []
+        
+        mycollection = mongohd.wf_do
+        
+        myStartDate = dateutil.parser.parse(myargs.get("start"))
+        myEndDate = dateutil.parser.parse(myargs.get("end"))
+        myLat = float(myargs.get("minlat"))
+        myLon = float(myargs.get("minlon"))
+        myLatX = float(myargs.get("maxlat"))
+        myLonX = float(myargs.get("maxlon"))
+
+        #myfirstvalue = mycollection.objects.raw({"dc_coverage_x": { "$gte" : myLat }, "dc_coverage_y": { "$gte" : myLon }, "dc_coverage_x": { "$lte" : myLatX }, "dc_coverage_y": { "$lte" : myLonX },"dc_coverage_t_min": { "$gte" : myStartDate },"dc_coverage_t_max": { "$lte" : myEndDate }})
+        myfirstvalue = mongohd.wf_do.objects.all()
+        
+
+        for document in myfirstvalue:
+             myLine = {
+                "fileId" : document.fileId,
+                "dc_identifier" : document.dc_identifier, 
+                "dc_coverage_x" : document.dc_coverage_x, 
+                "dc_coverage_y" : document.dc_coverage_y,
+                "dc_coverage_z" : document.dc_coverage_z,
+                "dc_title" : document.dc_title,
+                "dc_subject" : document.dc_subject, 
+                "dc_creator" : document.dc_creator, 
+                "dc_contributor" : document.dc_contributor,
+                "dc_publisher" : document.dc_publisher,
+                "dc_type" : document.dc_type,
+                "dc_format" : document.dc_format, 
+                "dc_date" : document.dc_date, 
+                "dc_coverage_t_min" : document.dc_coverage_t_min,
+                "dc_coverage_t_max" : document.dc_coverage_t_max,
+                "dcterms_available" : document.dcterms_available, 
+                "dcterms_dateAccepted" : document.dcterms_dateAccepted, 
+                "dc_rights" : document.dc_rights,
+                "dcterms_isPartOf" : document.dcterms_isPartOf,
+                "irods_path" : document.irods_path
+             }
+             documentResult1.append(myLine)
+
+        print ("result")
+        print (documentResult1)
+    
+        return [documentResult1] # ["total objects result:"] 
+
+        """
+        # Write server logs, on different levels:
+        # very_verbose, verbose, debug, info, warning, error, critical, exit
+        log.info("Received a test HTTP request")
+
+        # Parse input parameters:
+        # NOTE: they can be caught only if indicated in swagger files
+        self.get_input()
+        # pretty print arguments obtained from the _args private attribute
+        log.pp(self._args, prefix_line='Parsed args')
+
+        # Activate a service handle
+        service_handle = self.get_service_instance(service_name)
+        log.debug("Connected to %s:\n%s", service_name, service_handle)
+
+        # Handle errors
+        if service_handle is None:
+            log.error('Service %s unavailable', service_name)
+            return self.send_errors(
+                message='Server internal error. Please contact adminers.',
+                # code=hcodes.HTTP_BAD_NOTFOUND
+            )
+
+        # Output any python structure (int, string, list, dictionary, etc.)
+        response = 'Hello world!'
+        return self.force_response(response)
+        """
---
+++ projects/b2stage/backend/tests/test_airodsmeta.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+Run unittests inside the RAPyDo framework
+"""
+
+from restapi.tests import BaseTests, API_URI, AUTH_URI
+from utilities import htmlcodes as hcodes
+from utilities.logs import get_logger
+
+log = get_logger(__name__)
+
+
+class TestAirodsMeta(BaseTests):
+
+    """ Quickstart:
+    - setUp and tearDown methods before and after each test
+    - one method inside this class for each functionality to test
+    - decide the order with the name: test_NUMBER_METHOD_FUNCTIONALITY
+    """
+
+    _main_endpoint = '/airodsmeta'
+
+    def test_01_GET_giveityourname(self, client):
+        """
+        testing a feature on your endpoint class
+        """
+
+        endpoint = API_URI + self._main_endpoint
+        log.info('*** Testing GET call on %s' % endpoint)
+
+        r = client.get(endpoint)  # If NO authorization required
+        # headers, _ = self.do_login(client, None, None)
+        # r = client.get(
+        #     endpoint,
+        #     headers=headers  # If authorization required
+        # )
+
+        # Assert what is right or wrong
+        self.assertEqual(r.status_code, hcodes.HTTP_OK_BASIC)
+        output = self.get_content(r)
+
+        # pretty print data obtained from API to check the content
+        # log.pp(output)
+        assert output == 'Hello world!'
