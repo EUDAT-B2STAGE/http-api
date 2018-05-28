2d9615854f55b1a4e71eec0ce51136a6426a9379
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.it>
2018-05-28 12:24:05 +0200

    Move to production async

--- projects/b2stage/backend/tasks/seadata.py
+++ projects/b2stage/backend/tasks/seadata.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from utilities import path
 from restapi.flask_ext.flask_celery import CeleryExt
+from b2stage.apis.commons.seadatacloud import Metadata as md
+
 from utilities.logs import get_logger
 
 log = get_logger(__name__)
@@ -11,39 +14,66 @@ celery_app = CeleryExt.celery_app
 @celery_app.task(bind=True)
 def move_to_production_task(self, batch_id, irods_path, myjson):
 
-    path = '/usr/share/batch'
+    mypath = '/usr/share/batch'
 
     with celery_app.app.app_context():
-
         ###############
         log.info("I'm %s" % self.request.id)
-        local_path = '%s/%s' % (path, batch_id)
-        log.warning("Vars:\n%s\n%s\n%s", local_path, irods_path, myjson)
+        local_path = path.join(mypath, batch_id, return_str=True)
+        # log.warning("Vars:\n%s\n%s\n%s", local_path, irods_path, myjson)
         # icom = celery_app.get_service(service='irods', user='httpapi')
         imain = celery_app.get_service(service='irods')
-        ifiles = imain.list(irods_path)
-        log.info('irods content: %s', ifiles)
 
         ###############
         from glob import glob
-        files = glob('%s/*' % local_path)
+        files = glob(path.join(local_path, '*', return_str=True))
         log.info(files)
+        from b2stage.apis.commons.b2handle import PIDgenerator
+        pmaker = PIDgenerator()
 
         ###############
         for element in myjson.get('parameters', {}).get('pids', {}):
-            current = element.get('temp_id')
-            local_element = '%s/%s' % (local_path, current)
-            log.info('Element: %s', element)
+            tmp = element.pop('temp_id')
+            current = path.last_part(tmp)
+            local_element = path.join(local_path, tmp, return_str=True)
+            # log.info('Element: %s', element)
             if local_element in files:
-                log.info('%s: found', current)
+                log.info('Found: %s', local_element)
             else:
-                log.error('%s: NOT found', current)
-
+                log.error('NOT found: %s', local_element)
+                continue
             ###############
             # 1. copy file (irods) - MAY FAIL?
+            ifile = path.join(irods_path, current, return_str=True)
+            imain.put(local_element, ifile)
+            log.debug("Moved: %s" % current)
+
+            ###############
             # 2. request pid (irule)
+            # strip directory as prefix
+            PID = pmaker.pid_request(imain, ifile)
+            log.info('PID: %s', PID)
+
+            ###############
             # 3. set metadata (icat)
+            metadata, _ = imain.get_metadata(ifile)
+            # log.pp(metadata)
+            for key in md.keys:
+                if key not in metadata:
+                    value = element.get(key, '***MISSING***')
+                    args = {'path': ifile, key: value}
+                    imain.set_metadata(**args)
+
+            ###############
+            # 4. remove the batch file?
+            # or move it into a "completed/" folder
+            # where we can check if it was already done?
             pass
+
+        ###############
+        ifiles = imain.list(irods_path)
+        log.info('irods content: %s', ifiles)
+        log.verbose("\n")
 
         ###############
         return files
