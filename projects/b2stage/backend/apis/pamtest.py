8c94d8ab211c503423c5f2bdd2ccace0a40ef6af
Mattia <m.dantonio81@gmail.com>
2018-09-15 06:47:46 +0200

    PAM test: user and pwd as input

--- projects/b2stage/backend/apis/pamtest.py
+++ projects/b2stage/backend/apis/pamtest.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from b2stage.apis.commons.cluster import ClusterContainerEndpoint as Endpoint
 from utilities.logs import get_logger
 
@@ -11,6 +10,14 @@ class PAMTest(Endpoint):
 
     def post(self):
 
-        imain = self.get_service_instance(service_name='irods')
+        jargs = self.get_input()
+        user = jargs.get('username')
+        password = jargs.get('password')
+
+        imain = self.get_service_instance(
+            service_name='irods',
+            user=user,
+            password=password
+        )
         out = imain.list()
         return out
