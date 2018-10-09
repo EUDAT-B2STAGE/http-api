c40cb16fc138fed49649285d822c625a444e5905
Mattia <m.dantonio81@gmail.com>
2018-10-09 13:15:26 +0200

    Testing with zip split size 2048

--- projects/b2stage/backend/tasks/seadata.py
+++ projects/b2stage/backend/tasks/seadata.py
@@ -22,7 +22,7 @@ from utilities.logs import get_logger, logging
 
 # Size in bytes
 # TODO: move me into the configuration
-MAX_ZIP_SIZE = 4096
+MAX_ZIP_SIZE = 2048
 ####################
 mybatchpath = '/usr/share/batches'
 myorderspath = '/usr/share/orders'
