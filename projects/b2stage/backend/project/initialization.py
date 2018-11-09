7f10d975ad73108c2e4bd40c93e9f0e43af56577
Mattia <m.dantonio81@gmail.com>
2018-11-09 07:56:08 +0100

    Working on project initialization

--- projects/b2stage/backend/project/initialization.py
+++ projects/b2stage/backend/project/initialization.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
+import os
+from utilities.logs import get_logger
+
+log = get_logger(__name__)
 
 
 class Initializer(object):
-    pass
+
+    def __init__(self, services):
+        log.critical(os.environ)
+        log.critical(services)
+        # # create user
+        # user = self.db.User(
+        #     email=username, name=username, surname='iCAT',
+        #     uuid=getUUID(), authmethod='irods', session=session,
+        # )
+        # # add role
+        # user.roles.append(
+        #     self.db.Role.query.filter_by(name=self.default_role).first())
