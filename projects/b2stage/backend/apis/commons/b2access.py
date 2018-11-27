8cc20b19699a1b86960ebb4471e2d5607e401b7f
Mattia <m.dantonio81@gmail.com>
2018-11-27 09:52:32 +0100

    Removed unnecessary information from post payload

--- projects/b2stage/backend/apis/commons/b2access.py
+++ projects/b2stage/backend/apis/commons/b2access.py
@@ -162,8 +162,8 @@ class B2accessUtilities(EndpointResource):
         refresh_data = {
             "grant_type": "refresh_token",
             # "client_id": client_id,
-            "client_secret": client_secret,
-            # "refresh_token": refresh_token,
+            # "client_secret": client_secret,
+            "refresh_token": refresh_token,
             "scope": 'USER_PROFILE'
         }
         auth_hash = b64encode(
