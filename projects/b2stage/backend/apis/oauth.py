ab63774079691fa827fb405213f533bd29bb49cd
Mattia <m.dantonio81@gmail.com>
2019-02-08 06:17:13 +0100

    Cleanup

--- projects/b2stage/backend/apis/oauth.py
+++ projects/b2stage/backend/apis/oauth.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-OAUTH inside EUDAT services
+OAUTH2 authentication with EUDAT services
 """
 
 from flask import url_for
 from restapi.flask_ext.flask_irods.client import IrodsException
 from restapi import decorators as decorate
-# from utilities import htmlcodes as hcodes
 from b2stage.apis.commons import PRODUCTION
-# from b2stage.apis.commons.b2access import B2accessUtilities
 from b2stage.apis.commons.endpoint import EudatEndpoint
 from utilities import htmlcodes as hcodes
 from utilities.logs import get_logger
@@ -86,74 +84,27 @@ class Authorize(EudatEndpoint):
                 errors=extuser
             )
 
-        # B2ACCESS WITH TOKENS AUTHENTICATION
-        # log.pp(b2access_user.data)
-
-        # distinguishedName is only defined in prod, not in dev and staging
-        # b2access_dn = b2access_user.data.get('distinguishedName')
-
-        # copied from auth/sqlalchemy:store_oauth2_user
-        # DN very strange: the current key is something like 'urn:oid:2.5.4.49'
-        # is it going to change?
-        # b2access_dn = None
-        # for key, _ in b2access_user.data.items():
-        #     if 'urn:oid' in key:
-        #         b2access_dn = b2access_user.data.get(key)
-
         b2access_email = b2access_user.data.get('email')
-        # log.info("B2ACCESS DN = %s", b2access_dn)
         log.info("B2ACCESS email = %s", b2access_email)
 
-        # imain = self.get_service_instance(service_name='irods')
         imain = self.get_main_irods_connection()
 
         irods_user = self.get_irods_user_from_b2access(imain, b2access_email)
-        # irods_user = imain.get_user_from_dn(b2access_dn)
 
         if irods_user is None:
             err = "B2ACCESS credentials (%s) do not match any user in B2SAFE" \
                 % b2access_email
             log.error(err)
             return self.send_errors(err)
-        # if irods_user is None:
-        #     err = "B2ACCESS credentials (%s) do not match any user in B2SAFE" \
-        #         % b2access_dn
-        #     log.error(err)
-        #     return self.send_errors(err)
 
         # Update db to save the irods user related to this user account
         auth.associate_object_to_attr(extuser, 'irodsuser', irods_user)
 
-        # B2ACCESS WITH CERTIFICATES AUTHENTICATION - no longer used
-        """
-        proxy_file = self.obtain_proxy_certificate(auth, extuser)
-        if proxy_file is None:
-            return self.send_errors(
-                "B2ACCESS CA is down", "Could not get certificate files")
-
-        # iRODS informations: get/set from current B2ACCESS response
-
-        irods_user = self.set_irods_username(imain, auth, extuser)
-
-        if irods_user is None:
-            return self.send_errors(
-                "B2ACCESS credentials (%s) " % extuser.certificate_dn +
-                "do not match any user inside B2SAFE namespace"
-            )
-        """
         user_home = imain.get_user_home(irods_user)
 
         # If all is well, give our local token to this validated user
         local_token, jti = auth.create_token(auth.fill_payload(intuser))
         auth.save_token(auth._user, local_token, jti)
-
-        # FIXME: Workout a better way to get the host in this example
-        # uri = self.httpapi_location(
-        #     request.url.replace("/auth/authorize", ''),
-        #     API_URL + "/namespace" + user_home
-        # )
-        # get_example = "curl -H 'Authorization: %s %s' %s" \
-        #     % ('Bearer', local_token, uri)
 
         # FIXME: Create a method to reply with standard Bearer oauth response
         # return self.send_credentials(local_token, extra, metas)
