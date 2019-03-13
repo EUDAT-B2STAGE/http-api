cfa9a241fcc475e1eb6a3ea19c1a400b2a131233
Mattia <m.dantonio81@gmail.com>
2019-03-13 06:53:35 +0100

    First experiments with pids head method

--- projects/b2stage/backend/apis/extended.py
+++ projects/b2stage/backend/apis/extended.py
@@ -11,6 +11,7 @@ https://github.com/EUDAT-B2STAGE/http-api/blob/master/docs/user/endpoints.md
 """
 
 from restapi.flask_ext.flask_irods.client import IrodsException
+from restapi.exceptions import RestApiException
 from b2stage.apis.commons.b2handle import B2HandleEndpoint
 from b2stage.apis.commons import CURRENT_HTTPAPI_SERVER, CURRENT_MAIN_ENDPOINT
 from b2stage.apis.commons import PUBLIC_ENDPOINT
@@ -28,20 +29,29 @@ log = get_logger(__name__)
 class PIDEndpoint(Uploader, Downloader, B2HandleEndpoint):
     """ Handling PID on endpoint requests """
 
-    def eudat_pid(self, pid):
+    def eudat_pid(self, pid, head=False):
 
         # recover metadata from pid
         metadata, bad_response = self.get_pid_metadata(pid)
         if bad_response is not None:
-            return bad_response
+            if head:
+                raise RestApiException(RestApiException=bad_response.code)
+            else:
+                return bad_response
         url = metadata.get('URL')
         if url is None:
-            return self.send_errors(
-                message='B2HANDLE: empty URL_value returned',
-                code=hcodes.HTTP_BAD_NOTFOUND)
+            error_code = hcodes.HTTP_BAD_NOTFOUND
+            if head:
+                raise RestApiException(RestApiException=error_code.code)
+            else:
+                return self.send_errors(
+                    message='B2HANDLE: empty URL_value returned', code=error_code)
 
         if not self.download_parameter():
-            return metadata
+            if head:
+                raise RestApiException(RestApiException=hcodes.HTTP_OK_BASIC)
+            else:
+                return metadata
         # download is requested, trigger file download
 
         rroute = '%s%s/' % (CURRENT_HTTPAPI_SERVER, CURRENT_MAIN_ENDPOINT)
@@ -58,19 +68,29 @@ class PIDEndpoint(Uploader, Downloader, B2HandleEndpoint):
             url = url.replace(proute, '/')
         else:
             # Otherwise, perform a request to an external service?
-            return self.send_warnings(
-                {'URL': url},
-                errors=[
-                    "Data-object can't be downloaded by current " +
-                    "HTTP-API server '%s'" % CURRENT_HTTPAPI_SERVER
-                ]
-            )
+            if head:
+                # HTTP_MULTIPLE_CHOICES ?
+                raise RestApiException(RestApiException=hcodes.HTTP_MULTIPLE_CHOICES)
+            else :
+                return self.send_warnings(
+                    {'URL': url},
+                    errors=[
+                        "Data-object can't be downloaded by current " +
+                        "HTTP-API server '%s'" % CURRENT_HTTPAPI_SERVER
+                    ]
+                )
 
         r = self.init_endpoint()
         if r.errors is not None:
-            return self.send_errors(errors=r.errors)
+            if head:
+                raise RestApiException(RestApiException=hcodes.HTTP_SERVER_ERROR)
+            else:
+                return self.send_errors(errors=r.errors)
         url = self.download_object(r, url)
-        return url
+        if head:
+            raise RestApiException(RestApiException=hcodes.HTTP_OK_BASIC)
+        else:
+            return url
 
     @decorate.catch_error(exception=IrodsException, exception_label='B2SAFE')
     def get(self, pid):
@@ -80,4 +100,10 @@ class PIDEndpoint(Uploader, Downloader, B2HandleEndpoint):
             from seadata.apis.commons.seadatacloud import seadata_pid
             return seadata_pid(self, pid)
         except ImportError:
-            return self.eudat_pid(pid)
+            return self.eudat_pid(pid, head=False)
+
+    @decorate.catch_error(exception=IrodsException, exception_label='B2SAFE')
+    def head(self, pid):
+        """ Get metadata or file from pid """
+
+        return self.eudat_pid(pid, head=True)
