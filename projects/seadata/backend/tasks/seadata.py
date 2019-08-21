f0a94644568ac6b516c4c30bf2082f6e08ff0e4f
Mattia <m.dantonio81@gmail.com>
2019-08-21 06:58:46 +0200

    Debug code

--- projects/seadata/backend/tasks/seadata.py
+++ projects/seadata/backend/tasks/seadata.py
@@ -26,7 +26,8 @@ from utilities.logs import get_logger, logging
 
 # Size in bytes
 # TODO: move me into the configuration
-MAX_ZIP_SIZE = 2147483648  # 2 gb
+# MAX_ZIP_SIZE = 2147483648  # 2 gb
+MAX_ZIP_SIZE = 100000
 ####################
 
 log = get_logger(__name__)
