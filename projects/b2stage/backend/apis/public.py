5e46dcf40e4ade098f80acf126744ee5ea982f82
Mattia <m.dantonio81@gmail.com>
2020-01-07 18:18:03 +0100

    Fix endpoint definition

--- projects/b2stage/backend/apis/public.py
+++ projects/b2stage/backend/apis/public.py
@@ -19,7 +19,7 @@ class Public(B2HandleEndpoint):
     labels = ['eudat', 'pids', 'public']
     depends_on = ['IRODS_ANONYMOUS', 'ENABLE_PUBLIC_ENDPOINT']
     GET = {
-        '/public/<path:location>': {
+        '/public/<location>': {
             'summary': 'Let non-authenticated users get data about a public data-object',
             'custom': {},
             'parameters': [
