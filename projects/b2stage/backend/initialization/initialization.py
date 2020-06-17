58ed5319c2009f77a9cf49ae156cb96cd60114f8
Mattia <m.dantonio81@gmail.com>
2020-06-17 14:25:09 +0200

    Added missing customizer classes

--- projects/b2stage/backend/initialization/initialization.py
+++ projects/b2stage/backend/initialization/initialization.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
 import os
 
 from restapi.utilities.logs import log
 
 
-class Initializer(object):
-
+class Initializer:
     def __init__(self, services, app=None):
 
-        sql = services.get('sqlalchemy', None)
+        sql = services.get("sqlalchemy", None)
 
         if sql is None:
             log.critical("Unable to retrieve sqlalchemy service")
@@ -17,3 +15,22 @@ class Initializer(object):
         if app is None:
             log.critical("Unable to retrieve app context")
             return
+
+
+class Customizer:
+    """
+    This class is used to manipulate user information
+    - custom_user_properties is executed just before user creation
+                             use this to removed or manipulate input properties
+                             before sending to the database
+    - custom_post_handle_user_input is used just after user registration
+                                    use this to perform setup operations,
+                                    such as verify default settings
+    """
+
+    def custom_user_properties(self, properties):
+        return properties
+
+    def custom_post_handle_user_input(self, auth, user_node, properties):
+
+        return True
