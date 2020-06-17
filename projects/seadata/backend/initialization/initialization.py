909f0b166a670518f1d750c11fa1af103ff6f4dd
Mattia <m.dantonio81@gmail.com>
2020-06-17 14:25:09 +0200

    Added missing customizer classes

--- projects/seadata/backend/initialization/initialization.py
+++ projects/seadata/backend/initialization/initialization.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
 import os
 
-from restapi.utilities.uuid import getUUID
 from restapi.utilities.logs import log
+from restapi.utilities.uuid import getUUID
 
 
-class Initializer(object):
-
+class Initializer:
     def __init__(self, services, app=None):
 
-        sql = services.get('sqlalchemy', None)
+        sql = services.get("sqlalchemy", None)
 
         if sql is None:
             log.critical("Unable to retrieve sqlalchemy service")
@@ -20,14 +18,14 @@ class Initializer(object):
             return
 
         with app.app_context():
-            users = os.getenv('SEADATA_PRIVILEGED_USERS')
+            users = os.getenv("SEADATA_PRIVILEGED_USERS")
 
             if users is None or users == "":
                 users = []
             else:
-                users = users.replace(' ', '').split(',')
+                users = users.replace(" ", "").split(",")
             # users = ['stresstest', 'svanderhorst']
-            roles = ['normal_user', 'staff_user']
+            roles = ["normal_user", "staff_user"]
             if len(users) == 0:
                 log.info("No privileged user found")
             else:
@@ -42,15 +40,33 @@ class Initializer(object):
                             "uuid": getUUID(),
                             "email": username,
                             "name": username,
-                            "surname": 'iCAT',
-                            "authmethod": 'irods',
+                            "surname": "iCAT",
+                            "authmethod": "irods",
                         }
                         user = sql.User(**userdata)
                         for r in roles:
-                            user.roles.append(
-                                sql.Role.query.filter_by(name=r).first())
+                            user.roles.append(sql.Role.query.filter_by(name=r).first())
                         sql.session.add(user)
                         sql.session.commit()
                         log.info("User {} created with roles: {}", username, roles)
                     except BaseException as e:
                         log.error("Errors creating user {}: {}", username, str(e))
+
+
+class Customizer:
+    """
+    This class is used to manipulate user information
+    - custom_user_properties is executed just before user creation
+                             use this to removed or manipulate input properties
+                             before sending to the database
+    - custom_post_handle_user_input is used just after user registration
+                                    use this to perform setup operations,
+                                    such as verify default settings
+    """
+
+    def custom_user_properties(self, properties):
+        return properties
+
+    def custom_post_handle_user_input(self, auth, user_node, properties):
+
+        return True
