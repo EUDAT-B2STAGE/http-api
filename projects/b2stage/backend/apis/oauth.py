fd4a54cf71486e51f5d0666daf54587298de42d1
Mattia <m.dantonio81@gmail.com>
2020-06-18 13:03:16 +0200

    Fixed deprecated access to auth._user

--- projects/b2stage/backend/apis/oauth.py
+++ projects/b2stage/backend/apis/oauth.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
-
 """
 OAUTH2 authentication with EUDAT services
 """
 
-from flask import url_for, request
-
 from b2stage.apis.commons import PRODUCTION
 from b2stage.apis.commons.endpoint import EudatEndpoint
-
-from restapi.exceptions import RestApiException
-from restapi.connectors.irods.client import IrodsException
+from flask import request, url_for
 from restapi import decorators
-
+from restapi.connectors.irods.client import IrodsException
+from restapi.exceptions import RestApiException
 from restapi.utilities.logs import log
 
 
@@ -22,13 +17,13 @@ class OauthLogin(EudatEndpoint):
     to ask the current user for authorization/token.
     """
 
-    baseuri = '/auth'
-    labels = ['eudat', 'b2access', 'authentication']
-    depends_on = ['B2ACCESS_APPKEY']
+    baseuri = "/auth"
+    labels = ["eudat", "b2access", "authentication"]
+    depends_on = ["B2ACCESS_APPKEY"]
     _GET = {
-        '/askauth': {
-            'summary': 'Redirection to B2ACCESS oauth2 login',
-            'responses': {'200': {'description': 'redirected'}},
+        "/askauth": {
+            "summary": "Redirection to B2ACCESS oauth2 login",
+            "responses": {"200": {"description": "redirected"}},
         }
     }
 
@@ -46,17 +41,16 @@ class OauthLogin(EudatEndpoint):
 
         if b2access is None:
             raise RestApiException(
-                "B2ACCESS integration is not enabled",
-                status_code=503
+                "B2ACCESS integration is not enabled", status_code=503
             )
 
-        authorized_uri = url_for('authorize', _external=True)
+        authorized_uri = url_for("authorize", _external=True)
         if PRODUCTION:
             # What needs a fix:
             # http://awesome.docker:443/auth/authorize
             # curl -i -k https://awesome.docker/auth/askauth
-            authorized_uri = authorized_uri.replace('http:', 'https:').replace(
-                ':443', ''
+            authorized_uri = authorized_uri.replace("http:", "https:").replace(
+                ":443", ""
             )
 
         log.info("Ask redirection to: {}", authorized_uri)
@@ -71,14 +65,14 @@ class Authorize(EudatEndpoint):
     Use the B2ACCESS token to retrieve info about the user
     """
 
-    baseuri = '/auth'
-    labels = ['eudat', 'b2access', 'authentication']
-    depends_on = ['B2ACCESS_APPKEY']
+    baseuri = "/auth"
+    labels = ["eudat", "b2access", "authentication"]
+    depends_on = ["B2ACCESS_APPKEY"]
     _GET = {
-        '/authorize': {
-            'summary': 'Produce internal token if B2ACCESS authorization is granted',
-            'responses': {
-                '200': {'description': 'REST API token from B2ACCESS authentication'}
+        "/authorize": {
+            "summary": "Produce internal token if B2ACCESS authorization is granted",
+            "responses": {
+                "200": {"description": "REST API token from B2ACCESS authentication"}
             },
         }
     }
@@ -95,9 +89,11 @@ class Authorize(EudatEndpoint):
         # Get b2access token
         auth = self.auth
         b2access = self.create_b2access_client(auth, decorate=True)
-        b2access_token, b2access_refresh_token, b2access_error = self.request_b2access_token(
-            b2access
-        )
+        (
+            b2access_token,
+            b2access_refresh_token,
+            b2access_error,
+        ) = self.request_b2access_token(b2access)
         if b2access_token is None:
             return self.send_errors(errors=b2access_error)
 
@@ -108,10 +104,10 @@ class Authorize(EudatEndpoint):
         if b2access_user is None and intuser is None:
             log.error(extuser)
             return self.send_errors(
-                errors='Unable to retrieve user info from b2access',
+                errors="Unable to retrieve user info from b2access",
             )
 
-        b2access_email = b2access_user.data.get('email')
+        b2access_email = b2access_user.data.get("email")
         log.info("B2ACCESS email = {}", b2access_email)
 
         imain = self.get_main_irods_connection()
@@ -120,24 +116,24 @@ class Authorize(EudatEndpoint):
 
         if irods_user is None:
             err = "B2ACCESS credentials ({}) do not match any user in B2SAFE".format(
-                b2access_email)
+                b2access_email
+            )
             log.error(err)
             return self.send_errors(err)
 
         # Update db to save the irods user related to this user account
-        self.associate_object_to_attr(extuser, 'irodsuser', irods_user)
+        self.associate_object_to_attr(extuser, "irodsuser", irods_user)
 
         user_home = imain.get_user_home(irods_user)
 
         # If all is well, give our local token to this validated user
         local_token, jti = auth.create_token(auth.fill_payload(intuser))
-        auth.save_token(auth._user, local_token, jti)
+        user = self.get_user()
+        auth.save_token(user, local_token, jti)
 
-        return self.response({
-            'token': local_token,
-            'b2safe_user': irods_user,
-            'b2safe_home': user_home,
-        })
+        return self.response(
+            {"token": local_token, "b2safe_user": irods_user, "b2safe_home": user_home}
+        )
 
 
 class B2accesProxyEndpoint(EudatEndpoint):
@@ -146,13 +142,13 @@ class B2accesProxyEndpoint(EudatEndpoint):
     using the stored b2access token (if still valid)
     """
 
-    baseuri = '/auth'
-    labels = ['eudat', 'b2access']
-    depends_on = ['B2ACCESS_APPKEY']
+    baseuri = "/auth"
+    labels = ["eudat", "b2access"]
+    depends_on = ["B2ACCESS_APPKEY"]
     _POST = {
-        '/proxy': {
-            'summary': 'Check and/or refresh current B2ACCESS proxy credentials',
-            'responses': {'200': {'description': 'refresh status'}},
+        "/proxy": {
+            "summary": "Check and/or refresh current B2ACCESS proxy credentials",
+            "responses": {"200": {"description": "refresh status"}},
         }
     }
 
