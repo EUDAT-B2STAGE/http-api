59adca01c7d9c7d7b263ae438f182b305a0275fb
Mattia <m.dantonio81@gmail.com>
2021-03-02 07:06:06 +0100

    Debug code

--- projects/b2stage/backend/initialization.py
+++ projects/b2stage/backend/initialization.py
@@ -1,10 +1,12 @@
-# from restapi.utilities.logs import log
+from typing import Optional
+
+from restapi.customizer import FlaskApp
 
 
 class Initializer:
-    def __init__(self, app=None):
+    def __init__(self, app: Optional[FlaskApp] = None):
         pass
 
     # This method is called after normal initialization if TESTING mode is enabled
-    def initialize_testing_environment(self):
+    def initialize_testing_environment(self) -> None:
         pass
--- projects/seadata/backend/initialization.py
+++ projects/seadata/backend/initialization.py
@@ -1,13 +1,15 @@
 import os
+from typing import Optional
 
 from restapi.connectors import sqlalchemy
+from restapi.customizer import FlaskApp
 from restapi.services.authentication import Role
 from restapi.utilities.logs import log
 from restapi.utilities.uuid import getUUID
 
 
 class Initializer:
-    def __init__(self, app=None):
+    def __init__(self, app: Optional[FlaskApp] = None):
 
         sql = sqlalchemy.get_instance()
 
@@ -15,46 +17,41 @@ class Initializer:
             log.critical("Unable to retrieve sqlalchemy service")
             return
 
-        if app is None:
-            log.critical("Unable to retrieve app context")
-            return
+        users = os.getenv("SEADATA_PRIVILEGED_USERS")
 
-        with app.app_context():
-            users = os.getenv("SEADATA_PRIVILEGED_USERS")
+        if users is None or users == "":
+            users = []
+        else:
+            users = users.replace(" ", "").split(",")
+        roles = [Role.USER, Role.STAFF]
+        if len(users) == 0:
+            log.info("No privileged user found")
+        else:
+            for username in users:
 
-            if users is None or users == "":
-                users = []
-            else:
-                users = users.replace(" ", "").split(",")
-            roles = [Role.USER, Role.STAFF]
-            if len(users) == 0:
-                log.info("No privileged user found")
-            else:
-                for username in users:
-
-                    if username == "":
-                        log.warning("Invalid username: [{}]", username)
-                        continue
-                    try:
-                        log.info("Creating user {}", username)
-                        userdata = {
-                            "uuid": getUUID(),
-                            "email": username,
-                            "name": username,
-                            "password": "not-used",
-                            "surname": "iCAT",
-                            "authmethod": "irods",
-                        }
-                        user = sql.User(**userdata)
-                        for r in roles:
-                            role = sql.Role.query.filter_by(name=r.value).first()
-                            user.roles.append(role)
-                        sql.session.add(user)
-                        sql.session.commit()
-                        log.info("User {} created with roles: {}", username, roles)
-                    except BaseException as e:
-                        log.error("Errors creating user {}: {}", username, str(e))
+                if username == "":
+                    log.warning("Invalid username: [{}]", username)
+                    continue
+                try:
+                    log.info("Creating user {}", username)
+                    userdata = {
+                        "uuid": getUUID(),
+                        "email": username,
+                        "name": username,
+                        "password": "not-used",
+                        "surname": "iCAT",
+                        "authmethod": "irods",
+                    }
+                    user = sql.User(**userdata)
+                    for r in roles:
+                        role = sql.Role.query.filter_by(name=r.value).first()
+                        user.roles.append(role)
+                    sql.session.add(user)
+                    sql.session.commit()
+                    log.info("User {} created with roles: {}", username, roles)
+                except BaseException as e:
+                    log.error("Errors creating user {}: {}", username, str(e))
 
     # This method is called after normal initialization if TESTING mode is enabled
-    def initialize_testing_environment(self):
+    def initialize_testing_environment(self) -> None:
         pass
